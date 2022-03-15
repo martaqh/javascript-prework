b7676f87e34244bb3c4dd444edc90d87489f8540
Marta Kurian-Herma <marta.k.herma@gmail.com>
2022-03-15 22:55:38 +0000

    Declare computerMove and playerMove variables in js code

---
+++ js/script.js
@@ -0,0 +1,5 @@
+var computerMove, playerMove;
+computerMove = 'kamień';
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+playerMove = 'papier';
+printMessage('Zagrałam ' + playerMove + '! Jeśli Twój ruch to nożyczki, to wygrywasz!');
