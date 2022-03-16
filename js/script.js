836d7155eaeddd2db5162bdbd976e8be2afa82f5
Marta Kurian-Herma <marta.k.herma@gmail.com>
2022-03-16 21:56:38 +0000

    Add computer moves logic

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
-var computerMove, playerMove;
-computerMove = 'kamień';
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
-playerMove = 'papier';
-printMessage('Zagrałam ' + playerMove + '! Jeśli Twój ruch to nożyczki, to wygrywasz!');
+let computerMove, randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('wylosowana liczba to: ' + randomNumber);
+if (randomNumber == '1') {
+  computerMove = 'kamień';
+} else if (randomNumber == '2') {
+        computerMove = 'papier'
+    } else if (randomNumber == '3') {
+        computerMove = 'nożyczki'
+    } else {
+  computerMove = 'nieznany ruch';
+}
+printMessage('Mój ruch: ' + computerMove);
